39c580be8d470088f7d7f0aba7834b9ce7449be6
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-11 20:57:15 +0530

    MaheswariKoyyani : Cheapest Hotel

--- Hotel.java
+++ Hotel.java
@@ -1,26 +1,14 @@
 package com.capgemini.hotelreservation;
 
 public class Hotel {
-	private String name;
-	private int regularRate;
+	public String name;
+	public int regularRate;
 	
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getRegularRate() {
-		return regularRate;
-	}
-	public void setRegularRate(int regularRate) {
-		this.regularRate = regularRate;
-	}
 	
 	public Hotel(String name, int regularRate) {
-		setName(name);
-		setRegularRate(regularRate);
+		super();
+		this.name = name;
+		this.regularRate = regularRate;
 	}
 	
 	@Override
