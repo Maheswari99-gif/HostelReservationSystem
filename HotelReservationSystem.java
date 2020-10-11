064f97e98a8cbb43ede03c34d36752074ac04a72
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-11 18:44:27 +0530

    MaheswariKoyyani : Hotel Reservation System

---
+++ HotelReservationSystem.java
@@ -0,0 +1,11 @@
+package com.capgemini.hotelreservation;
+
+import java.util.*;
+
+public class HotelReservationSystem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+
+}
---
+++ HotelReservationTest.java
@@ -0,0 +1,14 @@
+package com.capgemini.hotelreservation;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HotelReservationTest {
+	 @Test
+	    public void shouldAnswerWithTrue()
+	    {
+	        assertTrue( true );
+	    }
+
+}
