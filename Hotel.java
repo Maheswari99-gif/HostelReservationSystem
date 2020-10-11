b686ecb369c3e277412b58736f8eca1a99e9f6bc
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-11 19:05:40 +0530

    Ability to add Hotel

---
+++ Hotel.java
@@ -0,0 +1,31 @@
+package com.capgemini.hotelreservation;
+
+public class Hotel {
+	private String name;
+	private int regularRate;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRegularRate() {
+		return regularRate;
+	}
+	public void setRegularRate(int regularRate) {
+		this.regularRate = regularRate;
+	}
+	
+	public Hotel(String name, int regularRate) {
+		setName(name);
+		setRegularRate(regularRate);
+	}
+	
+	@Override
+	public String toString() {
+		return "\nHotel \n[ Name=  " + name + "\nRegular Rate=  $" + regularRate + " ]";
+	}
+
+}
